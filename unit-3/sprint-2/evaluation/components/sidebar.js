6fbea9aaf537950f73ecb5f80ea44a69bb8a06ce
carynd <caryntech@gmail.com>
2021-09-13 14:25:22 +0530

    1

---
+++ unit-3/sprint-2/evaluation/components/sidebar.js
@@ -0,0 +1,13 @@
+function sidebar(){
+    return ` <div id=sidebar>
+    <img src="https://techcrunch.com/">
+    <div>
+    <h3><a>Login</a></h3>
+    <h3><a>search</a></h3>
+    </div>
+   
+    </div>
+    `
+
+}
+export default sidebar;
