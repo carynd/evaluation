c9c2619685185d1c0ca1911925d4ae6120f7f1ac
carynd <caryntech@gmail.com>
2021-11-08 14:26:29 +0530

    initial

---
+++ unit-4/sprint-4/evaluation/demo/src/main/java/com/example/demo/Entity/Book.java
@@ -0,0 +1,14 @@
+package com.example.demo.Entity;
+@Entity
+public class Book {
+
+    /*
+    id : 101,
+		name: "Skull Candy Headphones",
+    price: 10000,
+    brand: "Skull Candy",
+    category: "Headphones",
+		registration_number: "2gh856",
+		manufacturer_id: "MAN254
+     */
+}
